3f38cbb44881fe0f7feca27da3c69bd32df7c49b
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2020-11-11 17:53:46 +0900

    update 修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -17,9 +17,9 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if  prototype.update(prototype_params)
-      redirect_to show
+    @prototype = Prototype.find(params[:id])
+    if  @prototype.update(prototype_update)
+      redirect_to prototype_path
     else
       render :edit
     end
@@ -28,7 +28,7 @@ class PrototypesController < ApplicationController
   def destroy
     prototype = Prototype.find(params[:id])
     prototype.destroy
-    redirect_to root_path
+    redirect_to 
   end
 
   def new
@@ -45,6 +45,10 @@ class PrototypesController < ApplicationController
   end
 
   private
+
+  def prototype_update
+    params.require(:prototype).permit(:image, :title, :catch_copy, :concept)
+  end
 
   def move_to_index
     unless user_signed_in?
