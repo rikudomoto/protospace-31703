93ddd83c619373996e31c52795c37b21e587693a
rikudomoto <73264978+rikudomoto@users.noreply.github.com>
2020-11-11 14:29:57 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,58 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :new, :edit]
+  before_action :move_to_index,except: [:index,:show]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if  prototype.update(prototype_params)
+      redirect_to show
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+    if @prototype.save
+       redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+  def prototype_params
+   params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+end
